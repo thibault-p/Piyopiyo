ac9d0a2d501e71606329f9a681d8065b0c7428cd
mrlapinou <thibaultperson@gmail.com>
2013-11-17 22:26:34 +0100

    Rope

---
+++ Piyo/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+
+	public Vector2 Speed;
+	// Use this for initialization
+	void Start () {
+		Speed = new Vector2 (5, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 1 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 2 - Movement per direction
+		Vector3 movement = new Vector3(
+			Speed.x * inputX,
+			Speed.y * inputY,
+			0);
+		
+		// 3 - Relative to the time
+		movement *= Time.deltaTime;
+		
+		// 4 - Move the game object
+		transform.Translate(movement);
+	}
+}
